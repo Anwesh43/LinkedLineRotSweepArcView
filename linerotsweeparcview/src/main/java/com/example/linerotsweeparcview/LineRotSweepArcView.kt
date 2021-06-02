aa60d6c692c1ebef38dc9ef46b93a4a7613d3b44
Anwesh43 <anweshthecool0@gmail.com>
2021-06-02 06:20:00 +0530

    adding constants and libraries

--- linerotsweeparcview/src/main/java/com/example/linerotsweeparcview/LineRotSweepArcView.kt
+++ linerotsweeparcview/src/main/java/com/example/linerotsweeparcview/LineRotSweepArcView.kt
@@ -1,2 +1,27 @@
 package com.example.linerotsweeparcview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#00C853",
+    "#304FFE",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
