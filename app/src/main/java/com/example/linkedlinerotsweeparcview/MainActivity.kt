9247ef6e3007e89ebd27b4fb3da9f212d1cf4ac5
Anwesh43 <anweshthecool0@gmail.com>
2021-06-02 07:10:51 +0530

    adding fullscreen and view in activity

--- app/src/main/java/com/example/linkedlinerotsweeparcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedlinerotsweeparcview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedlinerotsweeparcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.linerotsweeparcview.LineRotSweepArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineRotSweepArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
